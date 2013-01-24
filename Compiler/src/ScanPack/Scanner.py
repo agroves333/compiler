ed8e775ddce36df1ae2daf01264844c1e3affc76
David Mallon <dmallon83@gmail.com>
2013-01-24 14:39:12 -0700

    Cleaned out repo to do fresh commit with no project files

---
+++ Compiler/src/ScanPack/Scanner.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 24, 2013
+
+@author: david
+'''
+
+class Scanner():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
