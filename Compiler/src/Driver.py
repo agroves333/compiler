d821beb366c67f1dcdd4b9076ecb2eff10ce398d
David Mallon <dmallon83@gmail.com>
2013-01-25 11:56:18 -0700

    Some beginning Driver/Scanner work

--- Compiler/src/Driver.py
+++ Compiler/src/Driver.py
@@ -3,6 +3,14 @@ Created on Jan 24, 2013
 
 @author: david
 '''
+from ScanPack.Scanner import Scanner
 
+def main():
+    fileName = raw_input('Enter an input filename: ')
+    
+    fileScanner = Scanner(fileName)
+    
+    fileScanner.scan()
+    
 if __name__ == '__main__':
-    pass
+    main()
--- Compiler/src/ScanPack/Scanner.py
+++ Compiler/src/ScanPack/Scanner.py
@@ -3,15 +3,32 @@ Created on Jan 24, 2013
 
 @author: david
 '''
+import sys
 
 class Scanner():
     '''
     classdocs
     '''
+    
 
-
-    def __init__(self):
+    def __init__(self, fileName):
         '''
         Constructor
         '''
+        self.fileName = fileName
+        
+    def scan(self):
+        '''
+        Main function to begin scanning process
+        '''  
+        try:
+            file = open(self.fileName, 'r')          
+                        
+            print "I am scanning " + repr(self.fileName) + " for Tokens"  
+            
+            file.close
+        except IOError:
+            print "File not found"    
+
+        
         
