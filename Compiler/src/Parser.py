7f1b52ce48a4b4cfb665684fbe499e8935c8c127
Josh Schlick <josh.schlick+github@webfilings.com>
2013-02-12 00:16:41 -0700

    did next 8 functions, changed numbering

--- Compiler/src/Parser.py
+++ Compiler/src/Parser.py
@@ -1,11 +1,7 @@
-'''
-Created on Feb 7, 2013
 
-@author: david
-'''
 from Scanner import Scanner
 
-class MyClass(object):
+class Parser(object):
     
     scanner = Scanner()
     lookahead = ''
@@ -13,202 +9,259 @@ class MyClass(object):
     # Constructor
     def __init__(self):
         pass
-    
-    
+
     def parse(self):
         
         self.lookahead = self.scanner.getNextToken()
+
+    def match(self, toMatch): pass
         
     #1
     def systemGoal(self):
-        if(self.lookahead == "MP_PROGRAM"):
+        if self.lookahead is "MP_PROGRAM":
             self.program()
         else:
             self.error()
     #2
-    def program(self): pass
+    def program(self):
+        if self.lookahead is "MP_PROGRAM":
+            self.programHeading()
+            self.match(';')
+            self.block()
+            self.match('.')
+        else:
+            self.error()
     
     #3
-    def programHeading(self): pass
+    def programHeading(self):
+        if self.lookahead is "MP_PROGRAM":
+            self.match("program")
+            self.programIdentifier()
+        else:
+            self.error()
     
     #4
-    def block(self): pass
+    def block(self):
+        if self.lookahead is "MP_VAR":
+            self.variableDeclarationPart()
+            self.procedureAndFunctionDeclarationPart()
+            self.statementPart()
+        else:
+            self.error()
     
     #5
-    def variableDeclarationPart(self): pass
+    def variableDeclarationPart(self):
+        if self.lookahead is "MP_VAR":
+            self.match("var")
+            self.variableDeclaration()
+            self.match(';')
+            self.variableDeclarationTail()
+        else:
+            self.error()
     
-    #6
-    def variableDeclarationTail(self): pass
-    
-    #7
-    def variableDeclaration(self): pass
+    #6 #7
+    def variableDeclarationTail(self):
+        if self.lookahead in ["MP_PROCEDURE", "MP_FUNCTION", "MP_BEGIN"]:
+            return
+        elif self.lookahead is "MP_IDENTIFIER":
+            self.variableDeclaration()
+            self.match(';')
+            self.variableDeclarationTail()
+        else:
+            self.error()
     
     #8
-    def type(self): pass
+    def variableDeclaration(self):
+        if self.lookahead is "MP_IDENTIFIER":
+            self.identifierList()
+            self.match(':')
+            self.type()
+        else:
+            self.error()
     
-    #9
-    def procedureAndFunctionDeclarationPart(self): pass
+    #9 #10 #11
+    def type(self):
+        if self.lookahead is "MP_INTEGER":
+            self.match("integer")
+        elif self.lookahead is "MP_FLOAT":
+            self.match("float")
+        #TODO: Boolean rule #11
+        else:
+            self.error()
     
-    #10
+    #12 #13 #14
+    def procedureAndFunctionDeclarationPart(self):
+        if self.lookahead is "MP_PROCEDURE":
+            self.procedureDeclaration()
+            self.procedureAndFunctionDeclarationPart()
+        elif self.lookahead is "MP_FUNCTION":
+            self.functionDeclaration()
+            self.procedureAndFunctionDeclarationPart()
+        elif self.lookahead is "MP_BEGIN":
+            return
+        else:
+            self.error()
+
+    #15
     def procedureDeclaration(self): pass
     
-    #11
+    #16
     def functionDeclaration(self): pass
     
-    #12
+    #17
     def procedureHeading(self): pass
     
-    #13
+    #18
     def functionHeading(self): pass
     
-    #14
+    #19 #20
     def optionalFormalParameterList(self): pass
     
-    #15
+    #21 #22
     def formalParameterSectionTail(self): pass
     
-    #16
+    #23 #24
     def formalParameterSection(self): pass
     
-    #17
+    #25
     def valueParameterSection(self): pass
     
-    #18
+    #26
     def variableParameterSection(self): pass
     
-    #19
+    #27
     def statementPart(self): pass
     
-    #20
+    #28
     def compoundStatement(self): pass
     
-    #21
+    #29
     def statementSequence(self): pass
     
-    #22
+    #30 #31
     def statementTail(self): pass
     
-    #23
+    #32 - #41
     def statement(self): pass
     
-    #24
+    #42
     def emptyStatement(self): pass
     
-    #25
+    #
     def readStatement(self): pass
     
-    #26
+    #
     def readParameterTail(self): pass
     
-    #27
+    #
     def readParameter(self): pass
     
-    #28
+    #
     def writeStatement(self): pass
     
-    #29
+    #
     def writeParameterTail(self): pass
     
-    #30
+    #
     def writeParameter(self): pass
     
-    #31
+    #
     def assignmentStatement(self): pass
     
-    #32
+    #
     def ifStatement(self): pass
     
-    #33
+    #
     def optionalElsePart(self): pass
     
-    #34
+    #
     def repeatStatement(self): pass
     
-    #35
+    #
     def whileStatement(self): pass
     
-    #36
+    #
     def forStatement(self): pass
     
-    #37
+    #
     def controlVariable(self): pass
     
-    #38
+    #
     def initialValue(self): pass
     
-    #39
+    #
     def stepValue(self): pass
     
-    #40
+    #
     def finalValue(self): pass
     
-    #41
+    #
     def procedureStatement(self): pass
     
-    #42
+    #
     def optionalActualParameterList(self): pass
     
-    #43
+    #
     def actualParameterTail(self): pass
     
-    #44
+    #
     def actualParameter(self): pass
     
-    #45
+    #
     def expression(self): pass
     
-    #46
+    #
     def optionalRelationalPart(self): pass
     
-    #47
+    #
     def relationalOperator(self): pass
     
-    #48
+    #
     def simpleExpression(self): pass
     
-    #49
+    #
     def termTail(self): pass
     
-    #50
+    #
     def optionalSign(self): pass
     
-    #51
+    #
     def addingOperator(self): pass
     
-    #52
+    #
     def term(self): pass
     
-    #53
+    #
     def factorTail(self): pass
     
-    #54
+    #
     def multiplyingOperator(self): pass
     
-    #55
+    #
     def factor(self): pass
     
-    #56
+    #
     def programIdentifier(self): pass
     
-    #57
+    #
     def variableIdentifier(self): pass
     
-    #58
+    #
     def procedureIdentifier(self): pass
     
-    #59
+    #
     def functionIdentifier(self): pass
     
-    #60
+    #
     def booleanExpression(self): pass
     
-    #61
+    #
     def ordinalExpression(self): pass
     
-    #62
+    #
     def identifierList(self): pass
     
-    #63
+    #
     def identifierTail(self): pass
     
      
