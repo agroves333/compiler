0b1579871a80293d867fff7297355cd9de8469f8
David Mallon <dmallon83@gmail.com>
2013-05-01 17:42:46 -0600

    Change "to" and "downto" to be inclusive

--- Compiler/src/Parser.py
+++ Compiler/src/Parser.py
@@ -517,10 +517,10 @@ class Parser(object):
             self.finalValue()
             self.analyzer.genPushId(ident_rec)
             if(step == "to"):
-                self.analyzer.genBoolean(">", ident_rec, expression_rec)
+                self.analyzer.genBoolean(">=", ident_rec, expression_rec)
                 self.analyzer.genBranchFalse(self.analyzer.getLabel() + 1)
             elif(step == "downto"):
-                self.analyzer.genBoolean("<", ident_rec, expression_rec)
+                self.analyzer.genBoolean("<=", ident_rec, expression_rec)
                 self.analyzer.genBranchFalse(self.analyzer.getLabel() + 1)
             self.match('MP_DO')
             self.statement()
